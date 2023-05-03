1b5e4dd27304a7b7058a07b743b25ed913107786
edennyk <edennyk@gmail.com>
2023-05-02 23:31:10 -0400

    added fetures

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react'
+import {v4 as uuidv4} from 'uuid';
 
 export default function AddTodo({onAdd}) {
-    const [text, setText] = useState();
-    const handleAddTodo =(e) => {
-        setTodo = e.target.value;
+    const [text, setText] = useState('');
+    const handleChange = (e) => setText(e.target.value);
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      onAdd({ id: uuidv4(), text, status:'active'});
+      // 텍스트 초기화
+      setText('');
     }
+  
   return (
     <div>
-        <form>
-            <input type="text" value={text} placeholder="Add Todo" />
-            <button onClick={handleAddTodo}>Add New</button>
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={text} placeholder="Add Todo" onChange={handleChange}/>
+            <button>Add New</button>
         </form>
     </div>
   )
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,27 +1,42 @@
 import React, { useState } from 'react'
 import AddTodo from '../AddTodo/AddTodo';
+import Todo from '../Todo/Todo';
 
 const InitTodoList = [
     {id:1, text:'cleanning room', status:'active'},
     {id:2, text:'walking with Jennie', status:'active'},
 ];
 
-export default function TodoList(){
+export default function TodoList({filter}){
     const [todos, setTodos] = useState(InitTodoList);
 
+    //  새롭게 추가 되는 todo를 받아서 새로운 todo를 추가 하면 된다
     const handleAdd = (todo) =>{
         // updates new todo at todos list
-        setTodos
+        setTodos([...todos, todo]);
     }
-   
+
+    const handleUpdate = (updated) => setTodos(todos.map(todo => todo.id === updated.id ? updated : todo));
+
+    const handleDelete = (deleted) => setTodos(todos.filter(todo => todo.id !== deleted.id));
+    
+    const filterd = getFilterdItems(todos, filter);
     return(
         <section>
             <ul>
-                {todos.map((item)=>(
-                     <li key={item.id}>{item.text}</li>
+                {filterd.map((item)=>(
+                     <Todo key={item.id} todo={item} onUpdate={handleUpdate} onDelete={handleDelete} />
                 ))}
             </ul>
+             {/* onAdd => add가 되면 callback 함수인 handleAdd를 호출 */}
             <AddTodo onAdd={handleAdd} />
         </section>
     )
+}
+
+
+function getFilterdItems(todos, filter){
+    if(filter === 'all') { return todos;}
+
+    return todos.filter((todo => todo.status === filter));
 }
